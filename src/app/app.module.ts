1eb9728ce39a9407388ae23559e7121c8ac20df0
renecal <rene.calfin@gmail.com>
2022-06-17 09:43:09 -0400

    crear subcomponente de gastos

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,16 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { GastosComponent } from './components/gastos/gastos.component';
 import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
+import { IngresarGastoComponent } from './components/gastos/ingresar-gasto/ingresar-gasto.component';
+import { ListarGastoComponent } from './components/gastos/listar-gasto/listar-gasto.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     GastosComponent,
-    IngresarPresupuestoComponent
+    IngresarPresupuestoComponent,
+    IngresarGastoComponent,
+    ListarGastoComponent
   ],
   imports: [
     BrowserModule,
