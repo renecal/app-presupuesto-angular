d644ff5f92d9da77fc41860595339c1f94132e88
renecal <rene.calfin@gmail.com>
2022-06-17 10:47:23 -0400

    ruta obj ingresarPresupuesto y gastos

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { GastosComponent } from './components/gastos/gastos.component';
+import { IngresarPresupuestoComponent } from './components/ingresar-presupuesto/ingresar-presupuesto.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'ingresarPresupuesto', component: IngresarPresupuestoComponent },
+  { path: 'gastos', component: GastosComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
