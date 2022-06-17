bcff02c32431a2194b8575eec29cc6eae10f61a1
renecal <rene.calfin@gmail.com>
2022-06-17 16:43:01 -0400

    variables, ngModel, agregarGasto(), ngIf alert

--- src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
+++ src/app/components/gastos/ingresar-gasto/ingresar-gasto.component.ts
@@ -6,10 +6,29 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./ingresar-gasto.component.css']
 })
 export class IngresarGastoComponent implements OnInit {
+  nombreGasto: string;
+  cantidad: number;
+  formularioIncorrecto: boolean;
+  textoIncorrecto: string;
 
-  constructor() { }
+
+  constructor() {
+    this.nombreGasto = '';
+    this.cantidad = 0;
+    this.formularioIncorrecto = false;
+    this.textoIncorrecto = 'Nombre del gasto o cantidad incorrecta';
+   }
 
   ngOnInit(): void {
+  }
+
+  agregarGasto(){
+    if(this.nombreGasto == '' || this.cantidad <= 0){
+      this.formularioIncorrecto = true;
+    }else{
+      this.formularioIncorrecto = false;
+    }
+
   }
 
 }
